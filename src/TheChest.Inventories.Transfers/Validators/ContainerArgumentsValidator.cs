f3c77123c348abf15ccf914909849fb309b14f93
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-17 10:32:15 -0300

    Adds Validators

---
+++ src/TheChest.Inventories.Transfers/Validators/ContainerArgumentsValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TheChest.Inventories.Containers.Interfaces;
+
+namespace TheChest.Inventories.Transfers.Validators
+{
+    internal static class ContainerArgumentsValidator
+    {
+        /// <summary>
+        /// Validates that the specified source and destination inventories are not null.
+        /// </summary>
+        /// <typeparam name="T">The type of items contained in the inventory.</typeparam>
+        /// <param name="source">The source inventory to validate.</param>
+        /// <param name="destination">The destination inventory to validate.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="source"/> or <paramref name="destination"/> is null.</exception>
+        internal static void Validate<T>(IInventory<T> source, IInventory<T> destination)
+        {
+            _ = source ?? throw new ArgumentNullException(nameof(source));
+            _ = destination ?? throw new ArgumentNullException(nameof(destination));
+        }
+    }
+}
---
+++ src/TheChest.Inventories.Transfers/Validators/ContainerIndexValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TheChest.Inventories.Containers.Interfaces;
+
+namespace TheChest.Inventories.Transfers.Validators
+{
+    /// <summary>
+    /// Provides static methods for validating inventory instances and item indices before performing operations.
+    /// </summary>
+    internal static class ContainerIndexValidator
+    {
+        /// <summary>
+        /// Validates that the specified inventory and index are suitable for operations.
+        /// </summary>
+        /// <typeparam name="T">The type of items contained in the inventory.</typeparam>
+        /// <param name="inventory">The inventory to validate.</param>
+        /// <param name="index">The zero-based index of the item to validate.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="inventory"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="index"/> is less than 0 or greater than or equal to the size of the inventory.</exception>
+        internal static void Validate<T>(IInventory<T> inventory, int index)
+        {
+            _ = inventory ?? throw new ArgumentNullException(nameof(inventory));
+            if (index < 0 || index >= inventory.Size)
+                throw new ArgumentOutOfRangeException(nameof(index), $"Index must be between 0 and {inventory.Size - 1}.");
+        }
+    }
+}
